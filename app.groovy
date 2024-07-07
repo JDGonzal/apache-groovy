c5ac31457ad7aa6fe5601aeff5d2fe8f074c9d55
juan.gonzalezp <jdgonzal2@gmail.com>
2024-07-07 15:49:04 -0500

    feat: Apache Groovy. Classes
    - Crear una clase vs, un Script.
    - Crear una clase y llamarla desde un script.
    
    #step_25

---
+++ app.groovy
@@ -0,0 +1,11 @@
+// create a new instance of a developer
+Developer dev = new Developer()
+dev.first = 'Juan'
+dev.setLast('Piza')
+
+// assign some languages
+dev.languages << 'Groovy'
+dev.languages << 'Java'
+
+// call some methods
+dev.work()
