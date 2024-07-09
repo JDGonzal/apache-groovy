e252f9d001dace83502270b7147c3bcb9cae84df
juan.gonzalezp <jdgonzal2@gmail.com>
2024-07-09 18:08:32 -0500

    feat: Apache Groovy. Ejercicio de Twitter - Solución sugerida
    - Crear una clase Tweet.
    - Crear un script Twtter y  correrlo.
    - Bono probar un error.
    
    #step_27

---
+++ twitterdemo/Twitter.groovy
@@ -0,0 +1,7 @@
+// Definimos la variable de la clase
+def tweet = new Tweet ("@juanpiza", "My First Tweet")
+// Llamamos los dos métodos antes de imprimir
+tweet.addToRetweets()
+tweet.addToFavorites()
+// Imprimimos dicha variable
+println tweet
